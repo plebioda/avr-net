99db3a1528b5c18cbfd2acf7bf719f0d115e5706
pawel.lebioda89@gmail.com <pawel.lebioda89@gmail.com@a018619c-9330-2494-4511-e33c901fd56f>
2012-02-25 12:26:32 +0000

    uart config file added
    git-svn-id: https://avr-net.googlecode.com/svn/trunk@5 a018619c-9330-2494-4511-e33c901fd56f

---
+++ src/arch/uart_config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012 by Paweł Lebioda <pawel.lebioda89@gmail.com>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _UART_CONFIG_H
+#define _UART_CONFIG_H
+
+#define UART_BAUD_index 2
+
+#endif //_UART_CONFIG_H
