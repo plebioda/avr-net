ef7737bcc8c28025d51c8a4bc4824205850c2869
pawel.lebioda89@gmail.com <pawel.lebioda89@gmail.com@a018619c-9330-2494-4511-e33c901fd56f>
2012-02-26 23:20:48 +0000

    udp config header added
    git-svn-id: https://avr-net.googlecode.com/svn/trunk@13 a018619c-9330-2494-4511-e33c901fd56f

---
+++ src/net/udp_config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012 by Paweł Lebioda <pawel.lebioda89@gmail.com>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _UDP_CONFIG_H
+#define _UDP_CONFIG_H
+
+#define UDP_SOCKET_MAX	4
+
+#endif //_UDP_CONFIG_H
