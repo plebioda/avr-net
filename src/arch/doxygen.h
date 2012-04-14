a9003d6606f557dad30eb8e74a62a48b13c4033d
pawel.lebioda89@gmail.com <pawel.lebioda89@gmail.com@a018619c-9330-2494-4511-e33c901fd56f>
2012-04-14 12:35:25 +0000

    doxygen: arch groups definitions
    git-svn-id: https://avr-net.googlecode.com/svn/trunk@96 a018619c-9330-2494-4511-e33c901fd56f

---
+++ src/arch/doxygen.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2012 by Paweł Lebioda <pawel.lebioda89@gmail.com>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/**
+* \defgroup arch Architecture specificic
+* @{
+*/
+/**
+* \defgroup spi SPI - Serial Peripheral Interface
+* Funcions for sending and receiving data to/from SPI bus.
+*/
+/**
+* \defgroup i2c I2C - Inter-Integrated Circuit ( TWI - Two Wire Interface )
+* Funcions for sending and receiving data to/from I2C bus.
+*/
+/**
+* \defgroup uart UART - Universal Asynchronous Receiver and Transmitter
+* Funcions for sending and receiving data to/from UART
+*/
+/**
+* \defgroup exmem External Memory Configuration
+*/
+/**
+* \defgroup interrupts Interrupts configuration
+*/
+/**
+* @}
+*/
