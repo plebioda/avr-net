820d07db241416f8ac6adc7d246b43387d3937e1
pawel.lebioda89@gmail.com <pawel.lebioda89@gmail.com@a018619c-9330-2494-4511-e33c901fd56f>
2012-10-14 15:07:26 +0000

    debug_conf.h file added
    git-svn-id: https://avr-net.googlecode.com/svn/trunk@114 a018619c-9330-2494-4511-e33c901fd56f

---
+++ src/debug_conf.h
@@ -0,0 +1,11 @@
+#ifndef _DEBUG_CONF_H
+#define _DEBUG_CONF_H
+
+/**
+ * Indicates whether FILE directive should be a basename of file name or
+ * full path to file.
+ */
+#define DBG_CFG_SHORT_FILE
+
+
+#endif //_DEBUG_CONF_H
